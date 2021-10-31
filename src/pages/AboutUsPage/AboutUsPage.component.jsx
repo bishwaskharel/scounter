bdb7dbb75db824d485ef2c5a98558d259deb667f
bishwaskharel <bishukharel98@gmail.com>
2021-10-31 21:32:24 +0545

    full screen

--- src/pages/AboutUsPage/AboutUsPage.component.jsx
+++ src/pages/AboutUsPage/AboutUsPage.component.jsx
@@ -4,14 +4,27 @@ function AboutUsPage() {
   return (
     <div className="aboutUsPage" style={{ height: "100vh" }}>
       <iframe
+        title="aboutUsPage"
         src="https://embedstream.me/india-vs-new-zealand-stream-2"
         width="100%"
-        height="600vh"
+        height="80%"
         scrolling="no"
         frameborder="0"
         allowfullscreen
         allowtransparency
         referrerpolicy="unsafe-url"
+        style={{
+          overflow: "hidden",
+          overflowx: "hidden",
+          overflowy: "hidden",
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          top: "0%",
+          left: "0px",
+          right: "0px",
+          bottom: "0px",
+        }}
       ></iframe>
     </div>
   );
